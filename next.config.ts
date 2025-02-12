e8c7695b7cd4fce260c6f618327ebdbb3855379d
learn__ <punyaawen@gmail.com>
2025-02-12 15:31:14 +0700

    werwewe

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,17 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    unoptimized: true, // Disable Image Optimization for static export
+    unoptimized: true, // Required for static export
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*", // Redirect all paths
-        destination: "https://your-firebase-app.web.app/:path*", // Firebase domain
-        permanent: true, // 308 Permanent Redirect
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/((?!_next|static|favicon.ico|robots.txt).*)",
+  //       destination: "https://portofolio-f8be3.web.app",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
